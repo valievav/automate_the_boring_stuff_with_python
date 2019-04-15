90bdf85e14e324bc83eb15f37063da68763d550c
Venus <valievavm@gmail.com>
2019-04-15 07:11:30 +0300

    Optimization to Chapter 7 password solution

--- Chapter 7/strong_password_detection.py
+++ Chapter 7/strong_password_detection.py
@@ -6,67 +6,48 @@ import string
 passwords = []
 
 # generating random valid and invalid passwords
-def valid_password():
-    p = ''.join(random.choices(string.ascii_letters + string.digits, k=8))
+def valid_pass(length):
+    p = ''.join(random.choices(string.ascii_letters + string.digits, k=length))
     passwords.append(p)
 
-def invalid_password_lower_number():
-    p = ''.join(random.choices(string.ascii_lowercase + string.digits, k=8))
+def invalid_pass_lower_letter_digit(length):
+    p = ''.join(random.choices(string.ascii_lowercase + string.digits, k=length))
     passwords.append(p)
 
-def invalid_password_upper_number():
-    p = ''.join(random.choices(string.ascii_uppercase + string.digits, k=8))
+def invalid_pass_upper_letter_digit(length):
+    p = ''.join(random.choices(string.ascii_uppercase + string.digits, k=length))
     passwords.append(p)
 
-def invalid_password_no_number():
-    p = ''.join(random.choices(string.ascii_letters, k=8))
+def invalid_pass_no_digit(length):
+    p = ''.join(random.choices(string.ascii_letters, k=length))
     passwords.append(p)
 
 # populating passwords list
-valid_password()
-invalid_password_lower_number()
-invalid_password_upper_number()
-invalid_password_no_number()
+valid_pass(8)
+valid_pass(11)
+invalid_pass_lower_letter_digit(3)
+invalid_pass_upper_letter_digit(8)
+invalid_pass_no_digit(9)
 
 # selecting password
 user_password = random.choice(passwords)
 
-# validating password against rules
-def password_length(password):
-    if len(password) < 7:
-        return False
-    else:
-        return True
-
-def lowercase_letter(password):
-    if re.search(r"[a-z]", password) is None:
-        return False
-    else:
-        return True
-
-def upper_case(password):
-    if re.search(r"[A-Z]", password) is None:
-        return False
-    else:
-        return True
-
-def digits(password):
-    if re.search(r"[0-9]", password) is None:
-        return False
-    else:
-        return True
+# regex rules
+lower_letter_regex = re.compile(r"[a-z]")
+upper_letter_regex = re.compile(r"[A-Z]")
+digits_regex = re.compile(r"[0-9]")
 
 # validation on meeting all the requirements
 def password_validator(password):
-    if password_length(password) is True \
-            and lowercase_letter(password) is True \
-            and upper_case(password) is True \
-            and digits(password) is True:
+    r = bool(len(password) > 7
+                  and lower_letter_regex.search(password)
+                  and upper_letter_regex.search(password)
+                  and digits_regex.search(password))
+    if r is True:
         print(f"{user_password} is valid password")
-        return password # added this line because it returned None with value
     else:
         print(f"{user_password} is NOT a valid password")
-        return password # added this line because it returned None with value
+
 
 password_validator(user_password)
 
