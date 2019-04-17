d6cc919b28301f0d0bd1b6500cf0b39f250469ed
Venus <valievavm@gmail.com>
2019-04-17 04:18:53 +0300

    Adding task description to the password solution

--- Chapter 7/strong_password_detection.py
+++ Chapter 7/strong_password_detection.py
@@ -1,3 +1,11 @@
+'''
+Write a function that uses regular expressions to make sure the password
+string it is passed is strong. A strong password is defined as one that is at
+least eight characters long, contains both uppercase and lowercase characters,
+and has at least one digit. You may need to test the string against multiple
+regex patterns to validate its strength.
+'''
+
 import re
 import random
 import string
