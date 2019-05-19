b686a96ee930ce0bb36c300914c76b72c806a99c
Venus <valievavm@gmail.com>
2019-05-20 00:40:35 +0300

    updated functions to return password instead of appending result to a global variable

--- Chapter 7 - Pattern matching with RegEx/strong_password_detection.py
+++ Chapter 7 - Pattern matching with RegEx/strong_password_detection.py
@@ -16,26 +16,30 @@ passwords = []
 # generating random valid and invalid passwords
 def valid_pass(length):
     p = ''.join(random.choices(string.ascii_letters + string.digits, k=length))
-    passwords.append(p)
+    return p
+
 
 def invalid_pass_lower_letter_digit(length):
     p = ''.join(random.choices(string.ascii_lowercase + string.digits, k=length))
-    passwords.append(p)
+    return p
+
 
 def invalid_pass_upper_letter_digit(length):
     p = ''.join(random.choices(string.ascii_uppercase + string.digits, k=length))
-    passwords.append(p)
+    return p
+
 
 def invalid_pass_no_digit(length):
     p = ''.join(random.choices(string.ascii_letters, k=length))
-    passwords.append(p)
+    return p
+
 
 # populating passwords list
-valid_pass(8)
-valid_pass(11)
-invalid_pass_lower_letter_digit(3)
-invalid_pass_upper_letter_digit(8)
-invalid_pass_no_digit(9)
+passwords.append(valid_pass(8))
+passwords.append(valid_pass(11))
+passwords.append(invalid_pass_lower_letter_digit(3))
+passwords.append(invalid_pass_upper_letter_digit(8))
+passwords.append(invalid_pass_no_digit(9))
 
 # selecting password
 user_password = random.choice(passwords)
@@ -45,10 +49,10 @@ lower_letter_regex = re.compile(r"[a-z]")
 upper_letter_regex = re.compile(r"[A-Z]")
 digits_regex = re.compile(r"[0-9]")
 
+
 # validation on meeting all the requirements
 def password_validator(password):
-    return bool(len(password) > 7
-                and len(password) < 21
+    return bool(7 < len(password) < 21
                 and lower_letter_regex.search(password)
                 and upper_letter_regex.search(password)
                 and digits_regex.search(password))
