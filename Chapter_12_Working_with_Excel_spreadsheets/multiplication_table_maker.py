b43fbb44d22a09fce5068b114c5d1883b7131014
Venus <valievavm@gmail.com>
2019-07-16 20:43:42 +0200

    quick fix

--- Chapter_12_Working_with_Excel_spreadsheets/multiplication_table_maker.py
+++ Chapter_12_Working_with_Excel_spreadsheets/multiplication_table_maker.py
@@ -2,6 +2,7 @@
 Create a program multiplicationTable.py that takes a number N from the command
 line and creates an N×N multiplication table in an Excel spreadsheet.
 Row 1 and column A should be used for labels and should be in bold.
+Example of the program: py multiplicationTable.py 6
 '''
 
 import openpyxl, os, sys
