6f8939d22ae40e438f6579c2fd913b25812242f8
Venus <valievavm@gmail.com>
2019-07-23 20:37:51 +0200

    adding complexity to the program (returning major datapoints only, validating params, returning values for specified number of days and for specific hour

--- Chapter_14_Working_with_CSV_files_and_JSON_data/quick_weather_Example_Task.py
+++ Chapter_14_Working_with_CSV_files_and_JSON_data/quick_weather_Example_Task.py
@@ -1,10 +1,12 @@
-import json, requests, pprint
+import json, requests, pprint, sys
 
 
-def weather_results(api_call):
+def weather_results(api_call, timestamp_hour=None, get_raw_data=False):
     """
-    Prints result of API call in JSON format.\n
+    Prints short and full data results from API JSON response.\n
     :param api_call: valid api call
+    :param timestamp_hour: int, value should be divisible by 3, optional param
+    :param get_raw_data: Boolean, optional param
     :return:
     """
 
@@ -14,10 +16,26 @@ def weather_results(api_call):
         json_data = json.loads(json_text)  # transform str to json
         return json_data
 
-    # solution using built-in json method - here to show better option
+    # solution using built-in json method - here to show better option (not used in program)
     def get_json_using_built_in_json_func(response):
         json_data = response.json(response)
         return json_data
+
+    # function for retrieving 3 major data points
+    def weather_brief_data(json_data, data_number):
+        print(f"Date: {json_data['list'][data_number]['dt_txt']}")
+        print(f"Temperature: {json_data['list'][data_number]['main']['temp']}")
+        print(f"Weather: {json_data['list'][data_number]['weather'][0]['description']}\n")
+
+    # check variables correctness
+    if number_of_results > 40 or number_of_results == 0:
+        print("Max number_of_results is 40 (5 days), min number_of_results is 8 (1 day).\n"
+              "Please update the value accordingly.")
+        sys.exit()
+
+    if timestamp_hour % 3 != 0:
+        print("Timestamp hour should be divisible by 3. Please update the value.")
+        sys.exit()
 
     # send api call
     api_response = requests.get(api_call)
@@ -26,16 +44,37 @@ def weather_results(api_call):
     # get response in json format
     json_data = get_json_using_json_libr(api_response)
 
-    # print raw data
-    print("Raw data:")
-    pprint.pprint(json_data)
+    # header data
+    print(f"Weather in {json_data['city']['name']}, {json_data['city']['country']} "
+          f"(population - {json_data['city']['population']}):\n")
+
+    timestamp = str(timestamp_hour)+":00:00"
+
+    # iterating through json datetime and printing short weather outlook
+    for data_number in range(number_of_results):
+        date_str = json_data['list'][data_number]['dt_txt']
+        if timestamp_hour:
+            if date_str.endswith(timestamp):
+                weather_brief_data(json_data, data_number)
+        else:
+            weather_brief_data(json_data, data_number)
+
+    # print complete raw data if requested
+    if get_raw_data:
+        print("-"*8+"\nRaw data:")
+        pprint.pprint(json_data)
 
 
-city_name = "Hong Kong"
-country_code = "hk"
+city_name = "Krakow"
+country_code = "pl"
 api_key = "f4903b072656d35aa82e06a04f1e5408"
-number_of_results = 5
-api_call = f"https://api.openweathermap.org/data/2.5/forecast?q={city_name},{country_code}&units=metric&cnt={number_of_results}&APPID={api_key}"
 
-weather_results(api_call)
+number_of_days = 5
+number_of_results = 8*number_of_days  # 8 is a number of timestamps returned for 1 day (weather is provided for every 3 hours)
+hour_to_retrieve = 9
+
+weather_api_call = f"https://api.openweathermap.org/data/2.5/forecast?q={city_name},{country_code}&units=metric&cnt={number_of_results}&APPID={api_key}"
+get_raw_data_flag = False
+
+weather_results(weather_api_call, hour_to_retrieve, get_raw_data_flag)
 
